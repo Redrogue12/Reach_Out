2e44cf14da5d21b7b905a2283725fd235457da3a
Edgar <exgc12@gmail.com>
2017-07-23 20:26:35 -0400

    use passport

--- server.js
+++ server.js
@@ -21,3 +21,26 @@ app.use(express.static('public'))
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(session({
+  secret: 'some secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new MongoStore({
+    url: DATABASE_URL,
+    collection: 'sessions'
+  })
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  } else {
+    console.log('Not on my watch')
+    res.redirect('/')
+  }
+}
