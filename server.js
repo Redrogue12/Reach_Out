6d512292864e1aa47be0190bf0e7246c422acaa0
Edgar <exgc12@gmail.com>
2017-07-23 20:51:17 -0400

    endpoints, connect/close server

--- server.js
+++ server.js
@@ -44,3 +44,53 @@ function ensureAuthenticated(req, res, next) {
     res.redirect('/')
   }
 }
+
+app.use('/users', userRouter)
+app.use('/events', eventRouter)
+
+app.get('/logout', userController.logout)
+
+app.use('*', (req, res) => {
+  res.status(404).json({message: 'Request not found'})
+})
+
+let server
+
+function runServer (databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err)
+      }
+      server = app.listen(port, () => {
+        console.log(`Server started on port ${port}`)
+        console.log(databaseUrl)
+        resolve()
+      })
+    .on('error', err => {
+      reject(err)
+    })
+    })
+  })
+}
+
+function closeServer () {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server')
+      server.close(err => {
+        if (err) {
+          reject(err)
+          return
+        }
+        resolve()
+      })
+    })
+  })
+}
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err))
+}
+
+module.exports = {app, runServer, closeServer}
